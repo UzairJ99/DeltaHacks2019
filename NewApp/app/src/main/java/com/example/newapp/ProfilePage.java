634c0629652dfe17df31f62754b0f24ac3a56452
Uzair Jawaid <uzair-jawaid@hotmail.com>
2019-01-27 20:43:13 -0500

    Updated profile page details

--- NewApp/app/src/main/java/com/example/newapp/ProfilePage.java
+++ NewApp/app/src/main/java/com/example/newapp/ProfilePage.java
@@ -3,12 +3,12 @@ package com.example.newapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class ProfilePage extends AppCompatActivity {
+public class ProfilePage extends AppCompatActivity
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
     }
-
 }
